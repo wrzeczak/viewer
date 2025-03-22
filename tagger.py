0d348c277049936ef44e55cca8c3ef2f71ab3341
Wrzeczak Gorzprzodrzorzeczów <wrzeczak@protonmail.com>
2025-03-22 14:57:32 -0500

    rework tagger

--- tagger.py
+++ tagger.py
@@ -5,6 +5,9 @@ from os import listdir
 from os.path import isfile, join
 from typing import List
 import argparse
+from time import perf_counter
+from math import floor
+import ctypes
 
 #------------------------------------------------------------------------------
 
@@ -13,168 +16,330 @@ exclude_tagged_files = False
 
 parser = argparse.ArgumentParser(prog="WRZ: Gallery Tagger")
 parser.add_argument("source_folder")
-parser.add_argument("-e", "--exclude_tagged_files", action="store_true")
+parser.add_argument("-e", "--exclude_tagged_files", action="store_true", default=False) # exclude images which are already tagged
+parser.add_argument("-d", "--print_debug_information", action="store_true", default=False) # print timing information, mostly
 
 args = parser.parse_args()
 
 source_folder = args.source_folder
 exclude_tagged_files = args.exclude_tagged_files
+print_debug_information = args.print_debug_information
+
+def debug_print(str):
+    if print_debug_information: print("WRZ: DEBUG:", str)
 
 #------------------------------------------------------------------------------
 
-files = [f for f in listdir(source_folder) if isfile(join(source_folder, f)) 
-         and f != "TAGSET"
-         and not f.endswith(".mp4") and not f.endswith(".tag")
-         and not f.endswith(".css") and not f.endswith(".html")]
+tic = perf_counter()
 
-folder_is_tagged = False
+included_image_filetypes = [".jpg", ".jpeg", ".gif", ".png", ".bmp"]
 
-for f in listdir(source_folder):
-    if f.endswith(".tag"):
-        folder_is_tagged = True
+filenames = [f for f in listdir(source_folder) if isfile(join(source_folder, f)) and any(f.endswith(e) for e in included_image_filetypes)]
 
-tag_files = []
-
-for f in listdir(source_folder):
-    if isfile(f"{source_folder}/{f}") and f.endswith(".tag"):
-        tag_files.append(f)
-
-#------------------------------------------------------------------------------
+tag_files = [f for f in listdir(source_folder) if isfile(join(source_folder, f)) and f.endswith(".tag")]
+tags = [t[:-4] for t in tag_files]
 
 if exclude_tagged_files:
     tagged_files = []
 
-    for t in tag_files:
-        with open(f"{source_folder}/{t}", "r", encoding="utf8", errors="ignore") as f:
-            lines = f.readlines()
+    for _t in tag_files:
+        with open(_t, "r", encoding="utf", errors="ignore") as t:
+            lines = t.readlines()
             for l in lines:
                 tagged_files.append(l.strip())
 
-    files = [f for f in files if f not in tagged_files]
+    filenames = [f for f in filenames if f not in tagged_files]
 
-dicts = [None] * len(files)
+filenames_full = [join(source_folder, f) for f in filenames]
 
-for i in range(len(files)):
-    dicts[i] = {
-        "filename": files[i],
-        "tags": []
-    }
+# else: show all files
+toc = perf_counter()
 
-if folder_is_tagged:
-    for t in tag_files:
-        with open(f"{source_folder}/{t}", "r", encoding="utf8", errors="ignore") as f:
-            lines = f.readlines()
-            for l in lines:
-                for d in dicts:
-                    if d["filename"] == l.strip():
-                        d["tags"].append(t[:-4])
-image_idx = 0
-
-exit_without_saving = True
+debug_print(f"Initial file loading took {toc - tic} seconds!")
 
 #------------------------------------------------------------------------------
 
-tags = []
+tic = perf_counter()
 
-with open(f"{source_folder}/TAGSET", "r") as f:
-    lines = f.readlines()
-    tags = [s.strip() for s in lines]
+debug_print(tags)
+debug_print(f"FILENAMES: {filenames}")
+
+# tagged without ".tag" suffix i.e. according to 'tags,' not 'tag_files'
+tag_dict = {}
+
+for t in tag_files:
+    debug_print(f"TAG FILEPATH: {t}")
+    with open(join(source_folder, t), "r", encoding="utf8", errors="ignore") as f:
+        files = [l.strip() for l in f.readlines()]
+        debug_print(f"FILES ASSOC. WITH TAG {t}: {files}")
+        tag_dict[t[:-4]] = files
+        # debug_print(tag_dict[t[:4]])
+
+image_dict = {}
+
+for f in filenames:
+    image_dict[f] = []
+    for t in tags:
+        if f in tag_dict[t]:
+            image_dict[f].append(t)
+
+debug_print(image_dict)
+
+# exit(0)
+
+toc = perf_counter()
+
+debug_print(f"File tag association took {toc - tic} seconds!")
 
 #------------------------------------------------------------------------------
 
-def wrz_append(d: dict, s: str):
-    if s not in d["tags"]:
-        d["tags"].append(s)
-    else:
-        d["tags"].remove(s)
+_button_labels = list("12345QWERTASDFGZXCVB")
 
-def __wrz_button(i: int, h: int, d: List[dict], s: str):
-    if GuiButton( [ 600, 10 + (h * 50), 200, 50 ], str.encode(s)):
-        wrz_append(d[i], s)
-        exit_without_saving = False
+button_dict = {}
 
-def wrz_button(h: int, s: str):
-    __wrz_button(image_idx % len(files), h, dicts, s)
+if len(tags) > len(_button_labels):
+    print("Too many tags! Fix this! Email me! wrzeczak@protonmail.com")
+    exit(1)
 
-def wrz_buttons(tags: List[str]):
-    for i in range(len(tags)):
-        wrz_button(i, tags[i])
+i = 0
+for t in tags:
+    button_dict[_button_labels[i]] = t
+    i += 1
 
-def wrz_tags(i: int, d: List[dict]) -> str:
-    return ", ".join(d[image_idx % len(files)]["tags"])
+button_labels = _button_labels[:i]
+
+debug_print(button_dict)
 
 #------------------------------------------------------------------------------
+
+WIDTH = 1
+HEIGHT = 1
+IMAGE_HEIGHT = 1
+UI_HEIGHT = 1
 
 InitWindow(810, 650, b"WRZ: Image Tagger")
 
+WIDTH = floor(GetMonitorWidth(GetCurrentMonitor()) / 2)
+HEIGHT = floor(GetMonitorHeight(GetCurrentMonitor()) / 2)
+
+SetWindowSize(WIDTH, HEIGHT)
+SetWindowPosition(floor(WIDTH / 2), floor(HEIGHT / 2))
+
+IMAGE_HEIGHT = floor(0.7 * HEIGHT)
+LABEL_HEIGHT = 45
+UI_HEIGHT = HEIGHT - IMAGE_HEIGHT - LABEL_HEIGHT
+
+BUTTON_HEIGHT = floor(UI_HEIGHT / 7)
+BUTTON_WIDTH = floor(WIDTH / 6)
+BUTTON_X_OFFSET = floor(BUTTON_WIDTH / 2)
+BUTTON_Y_OFFSET = BUTTON_HEIGHT + IMAGE_HEIGHT + LABEL_HEIGHT
+
 SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()))
 
-image = LoadTexture(str.encode(f"{source_folder}/{files[image_idx % len(files)]}"))
+SetExitKey(KEY_NULL)
+exit_dialog = False # once set true, will show a dialog asking the user if they want to save changes
+save_changes = True
 
-# source_rect = ffi.new("Rectangle", [ 0, 0, image.width, image.height ])
-# dest_rect = ffi.new("Rectangle", [ 0, 0, 500, 500 ])
-# origin_vector = ffi.new("Vector2", 0, 0 )
+new_tag_dialog = False
+text_input = ffi.new("char*", b"\0")
+
+background_texture = LoadTexture(b"background.png")
+
+#------------------------------------------------------------------------------
+
+image_idx = 0
+
+debug_print(len(filenames))
+
+image = filenames[image_idx]
+tex = LoadTexture(str.encode(filenames_full[image_idx]))
+image_tags = image_dict[image]
+
+texture_load_error = False
+
+SetWindowTitle(str.encode(f"WRZ: Image Tagger - \"{image}\""))
+
+
+#------------------------------------------------------------------------------
 
 while not WindowShouldClose():
 
-    if IsKeyPressed(KEY_LEFT):
-        UnloadTexture(image)
-        image_idx -= 1
-        image = LoadTexture(str.encode(f"{source_folder}/{files[image_idx % len(files)]}"))
+    #------------------------------------------------------------------------------
+    
+    reload_picture = False
 
-    if IsKeyPressed(KEY_RIGHT):
-        UnloadTexture(image)
-        image_idx += 1
-        image = LoadTexture(str.encode(f"{source_folder}/{files[image_idx % len(files)]}"))
+    if IsKeyPressed(KEY_RIGHT) and not exit_dialog:
+        reload_picture = True
+        image_idx = (image_idx + 1) % len(filenames)        
+    
+    if IsKeyPressed(KEY_LEFT) and not exit_dialog:
+        reload_picture = True
+        image_idx = (image_idx - 1) % len(filenames)
+    
+    if reload_picture:
+        tic = perf_counter()
 
-    for i in range(0, 10):
-        if IsKeyPressed(KEY_ONE + i):
-            try:
-                wrz_append(dicts[image_idx % len(files)], tags[i])
-            except: continue
-            exit_without_saving = False
+        UnloadTexture(tex)
+        image = filenames[image_idx]
+        tex = LoadTexture(str.encode(filenames_full[image_idx]))
 
+        image_tags = image_dict[image]
+
+        toc = perf_counter()
+
+        debug_print(f"Image reloading took {toc - tic} seconds! Loaded @ {tex.width}x{tex.height} with tags {image_tags}")
+
+        if (tex.height * tex.width) == 0:
+            texture_load_error = True
+            debug_print(f"Texture Load Error! tex.width * tex.height == {tex.width * tex.height}")
+        else:
+            texture_load_error = False
+        
+        SetWindowTitle(str.encode(f"WRZ: Image Tagger - \"{image}\""))
+    
+    if IsKeyPressed(KEY_ESCAPE):
+        if IsKeyDown(KEY_LEFT_SHIFT):
+            save_changes = True
+            break
+
+        if new_tag_dialog: new_tag_dialog = False
+        else: exit_dialog = not exit_dialog
+
+    if IsKeyPressed(KEY_N):
+        new_tag_dialog = True
+    
+    key = chr(GetKeyPressed())
+
+    if key != chr(0) and not new_tag_dialog: 
+        debug_print(f"{key} [{ord(key)}]")
+
+        if key in button_labels:
+            tag = button_dict[key]
+
+            if tag in image_tags:
+                image_tags.remove(tag)
+                debug_print(f"{image} - {image_tags}")
+            else:
+                image_tags.append(tag)
+                debug_print(f"{image} - {image_tags}")
+
+
+    #------------------------------------------------------------------------------
+    
     BeginDrawing()
 
     ClearBackground(BLACK)
 
-    # DrawTexture(image, 10, 10, WHITE)
+    # DrawTexturePro(background_texture, [0, 0, background_texture.width, background_texture.height], [0, 0, WIDTH, HEIGHT], [0, 0], 0.0, WHITE)
 
-    DrawRectangle(70, 10, 520, 520, GRAY)
-    DrawTexturePro(image, [ 0, 0, image.width, image.height ], [ 0, 0, 500, 500 ], [ -80, -20 ], 0, WHITE)
+    DrawRectangle(0, IMAGE_HEIGHT + LABEL_HEIGHT, WIDTH, UI_HEIGHT, DARKGRAY)
+    # DrawRectangle(0, IMAGE_HEIGHT, WIDTH, LABEL_HEIGHT, WHITE)
 
-    for i in range(len(tags)):
-        GuiButton([10, 10 + (i * 50), 50, 50], str.encode(str(i)))
+    DrawText(str.encode(str(image_tags)), 10, IMAGE_HEIGHT + 8, 30, WHITE)
 
-    if not exit_without_saving:
-        if GuiButton([70, 570, 520, 50], b"#152# EXIT WITHOUT SAVING"):
-            exit_without_saving = True
-            CloseWindow()
+    if texture_load_error == False:
+        h = IMAGE_HEIGHT - 20
+        # h/y = w/x -> h * x / y = w
+        w = floor((h * tex.width) / tex.height)
 
-    wrz_buttons(tags)
+        DrawRectangle(floor((WIDTH - w - 30) / 2), 5, floor(w + 10), h + 10, WHITE)
+        DrawTexturePro(tex, [ 0, 0, tex.width, tex.height ], [ 0, 0, w, h ], [ -1 * ((WIDTH - w - 20) / 2), -10 ], 0, WHITE)
+    else:
+        m1 = str.encode(f"Error Loading File!!! w = {tex.width}, h = {tex.height}")
+        m2 = str.encode(image)
 
-    DrawText(str.encode(wrz_tags(image_idx, dicts)), 70, 540, 20, WHITE)
+        font_size = 30
+        w1 = MeasureText(m1, font_size)
+        w2 = MeasureText(m2, font_size)
+
+        DrawText(m1, floor((WIDTH - w1) / 2), floor((IMAGE_HEIGHT / 2) - font_size), font_size, WHITE)
+        DrawText(m2, floor((WIDTH - w2) / 2), floor(IMAGE_HEIGHT / 2), font_size, WHITE)
+
+    i = 0
+    for l in button_labels:
+        x = i % 5
+        y = (i - x) / 4
+
+        GuiButton([(x * BUTTON_WIDTH) + BUTTON_X_OFFSET, (y * BUTTON_HEIGHT) + BUTTON_Y_OFFSET, BUTTON_WIDTH, BUTTON_HEIGHT ], str.encode(f"{l} - {button_dict[l]}"))
+
+        i += 1
+    
+    if exit_dialog:
+        DrawRectangle(0, 0, WIDTH, HEIGHT, Fade(WHITE, 0.7))
+        exit_dialog_result = GuiMessageBox([floor(WIDTH / 4), floor(HEIGHT / 4), floor(WIDTH / 2), floor(HEIGHT / 2)], GuiIconText(ICON_EXIT, str.encode("Close Window")), str.encode("Do you want to exit and save changes?"), str.encode("Exit With Changes;Exit Without Saving;Cancel"))
+        
+        match(exit_dialog_result):
+            case 0: # close window button
+                debug_print("User Cancelled Exit Dialog (X)")
+                exit_dialog = False
+            case 1:
+                debug_print("User Exited (Save Changes)")
+                save_changes = True
+                break;
+            case 2:
+                debug_print("User Exited (Do Not Save Changes)")
+                save_changes = False
+                break;
+            case 3:
+                debug_print("User Cancelled Exit Dialog (Cancel)")
+                exit_dialog = False
+    
+    if new_tag_dialog:
+        DrawRectangle(0, 0, WIDTH, HEIGHT, Fade(WHITE, 0.7))
+
+        result = GuiTextInputBox([floor(WIDTH / 4), floor(HEIGHT / 4), floor(WIDTH / 2), floor(HEIGHT / 2)], str.encode("Create New Tag"), str.encode("Type new tag name - must result in a valid filename!"), str.encode("Ok;Cancel"), text_input, 24, ffi.new("bool*", True))
+
+        if result == 1: # "Ok"
+            new_tag = ffi.string(text_input).decode("utf-8").strip()
+
+            if new_tag not in tags:
+                debug_print(new_tag)
+                debug_print(tags)
+                debug_print(tag_files)
+
+                if len(tags) < len(_button_labels):
+                    button_dict[_button_labels[len(button_labels)]] = new_tag
+                    tags.append(new_tag)
+                    tag_files.append(join(new_tag, ".tag"))
+                    open(f"{source_folder}\\{new_tag}.tag", "w").close() # touch the new tag file
+                
+                debug_print(tags)
+                debug_print(tag_files)
+
+            exit(2)
+        elif result == 0 or result == 2:
+            new_tag_dialog = False
+            text_input = ffi.new("char*", b"\0")
+
 
     EndDrawing()
 
-if not exit_without_saving:
-    print(f"Exiting... Writing {len(tags)} tag(s)...")
+#------------------------------------------------------------------------------
+
+if save_changes:
+    debug_print("Saving changes...")
+
+    debug_print("Old tag dictionary:")
+    debug_print(tag_dict)
+
+    new_tag_dict = {}
+
     for t in tags:
-        print(f"Writing {t}.tag... ", end='')
-        with open(f"{source_folder}/{t}.tag", "a") as f:
-            counter = 0
-            for d in dicts:
-                if t in d["tags"]:
-                    f.write(f"{d["filename"]}\n")
-                    counter += 1
+        new_tag_dict[t] = []
 
-        unduplicated_lines = []
+    for n in filenames:
+        s = image_dict[n]
+        # debug_print(f"File {n} is tagged with {s}")
+        for t in tags:
+            if t in s:
+                new_tag_dict[t].append(f"{n}\n")    
+    
+    debug_print("New tag dictionary:")
+    debug_print(new_tag_dict)
 
-        with open(f"{source_folder}/{t}.tag", "r") as f:
-            lines = f.readlines()
-            unduplicated_lines = list(set(lines))
+    for t in tags:
+        debug_print(f"Writing {len(new_tag_dict[t])} files to {t}.tag...")
+        with open(f"{source_folder}\\{t}.tag", "w") as f:
+            f.writelines(new_tag_dict[t])
 
-        with open(f"{source_folder}/{t}.tag", "w") as f:
-            f.writelines(unduplicated_lines)
-            
-        print(f"wrote {counter} filename(s).")
+        
